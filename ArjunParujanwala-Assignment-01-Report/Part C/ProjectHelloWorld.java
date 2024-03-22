164402a1a10ac09a74a4631635f5b53b89e1035e
Arjun-Parujanwala <120456733+Arjun-Parujanwala@users.noreply.github.com>
2024-03-22 15:09:42 -0700

    Add files via upload

---
+++ ArjunParujanwala-Assignment-01-Report/Part C/ProjectHelloWorld.java
@@ -0,0 +1,14 @@
+/****************************************************************
+ *
+ * File: ProjectHelloWorld
+ * By: Arjun Parujanwala
+ * Date: 02-02-2024
+ *
+ * Description: Hello World.
+ *
+ ****************************************************************/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
---
+++ ArjunParujanwala-Assignment-01-Report/Part F/ProjectHelloWorldPro.java
@@ -0,0 +1,20 @@
+/****************************************************************
+ *
+ * File: ProjectHelloWorldPro
+ * By: Arjun Parujanwala
+ * Date: 02-02-2024
+ *
+ * Description: Hello World.
+ *
+ ****************************************************************/
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World! Discussion Sections are here!!!\n" +
+                           "Course:          CSC 215.TA and CSC 215.\n" +
+                           "Student:         Arjun Parujanwala" +
+                           "Teammate:        Troy Mueller\n" +
+                           "Assignment #:    01\n" +
+                           "Due Date & Time: 02-05-2024 at 11:59 PM\n" +
+                           "SFSU's motto:    'Experientia Docet' means Experience Teaches\n");
+    }
+}
